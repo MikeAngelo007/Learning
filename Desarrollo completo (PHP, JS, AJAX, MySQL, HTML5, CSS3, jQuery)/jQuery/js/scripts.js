b929a43891eb12de9167a0db8022e2d884cd3bb2
Miguel Tovar <matovaro@unal.edu.co>
2018-08-08 00:28:06 -0500

    Avances en jQuery

--- Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/jQuery/js/scripts.js
+++ Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/jQuery/js/scripts.js
@@ -15,9 +15,42 @@ document.addEventListener('DOMContentLoaded',function(event){
 $(function(){
     'use strict';
 
-    $('#menu').on('click',function(){
+
+    //$('main').children()[1].childNodes[1].innerText="Test"; // Para tener una idea de como se cambian los valores.
+    /* $('main article:first h2').text(); // Ver el texto en el h2
+    $('main article:first h2').text('Hola'); // Modificar el texto en el h2
+    $('main article:first h2').html('<h1>Londres 2018</h1>); // Modificar el texto en el h2 */
+    /* $('main').on({
+        click:function(){
+            $(this).addClass('fondorojo activo');
+        },mouseenter:function(){
+            $(this).addClass('fondorojo');
+        },mouseleave:function(){
+            $(this).addClass('activo');
+        }
+    }) */
+
+    //$('.navegacion').children().children().children(); // Obtener los hijos de un elemento
+   // $('article:first').parent();
+    //$('article:first').parent().children()[1] // Recorremos los elementos hijo como un arreglo y asi podemos acceder a cad auno de los elementos.
+    //$('article:first').next().next(); // Seleccionamos el siguiente elemento.
+
+    //$('div.logo img').addClass('activo');
+
+    $('.navegacion').show();
+
+    //$('.navegacion nav ul li:first').addClass('activo');
+    //$('main article:first').addClass('activo');
+
+    /* $('.navegacion ul li a').on('click',function(e){
+        $('.navegacion ul li a').removeClass('activo');
+        e.preventDefault();
+        $(this).addClass('activo');
+    }); */
+    
+    /* $('#menu').on('click',function(){
         $('#navegacion').show(); // show es lo contrario a hide.
-    });
+    }); */
     
     /* $('div.logo img').on('click', function(){
         $(this).remove();
