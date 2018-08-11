4842550c937af699c658e34429471961030f20ad
Miguel Tovar <matovaro@unal.edu.co>
2018-08-11 17:52:17 -0500

    Plugins 1

--- Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/Big Project/js/main.js
+++ Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/Big Project/js/main.js
@@ -170,6 +170,7 @@
 
 
 $(function(){
+    //Programa de conferencias
     $('.programa-evento .info-curso:first').show();
     $('.menu-programa a:first').addClass('activo');
     $('.menu-programa a').on('click',function(){
@@ -182,4 +183,12 @@ $(function(){
 
         return false; //Esto evita que la pagina salte al id.
     });
+
+
+    //Animaciones para los numeros
+
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6},1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15},1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3},1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9},1500);
 });
