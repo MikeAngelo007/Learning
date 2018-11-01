e4442c2c9abfeb550e9896aac97d6e3744d2988d
Miguel Tovar <matovaro@unal.edu.co>
2018-10-31 22:17:05 -0500

    Heroku include

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <div class="contenedor">
+      <h1>Aprendiendo PHP</h1>
+
+        <div class="contenido">
+            <?php
+              echo 'Probado 1...2..3';
+            ?>
+              
+        </div>
+    </div>
+
+    <a href="Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)\Big Project\index.html"> Big Project</a>
+
+
+
+
+  </body>
+</html>
