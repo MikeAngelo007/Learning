57c9b8d47e500a720c7256359d769ac8b79608df
Miguel Tovar <matovaro@unal.edu.co>
2018-11-12 00:41:20 -0500

    ADVANCE

--- index.php
+++ index.php
@@ -3,32 +3,43 @@
   <head>
     <meta charset="utf-8">
     <title>Mis proyectos</title>
-    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
 
-    <h1>Mis proyectos</h1>
+            <?php
+              echo '<h1>Mis proyectos</h1>';
+            ?>
+
+    
 
     <div class="contenedor">
       
 
         <div class="contenido">
-            <?php
-              echo 'Probado 1...2..3';
-            ?>
+            
               
         </div>
         <div class="proyecto">
-        <!-- <a href="https://bit.ly/2QB3hoH">GDLWEBCAMP (Proyecto en desarrollo)</a> -->
-        <a href="Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)\Big Project\index.html">GDLWEBCAMP (Proyecto en desarrollo)</a>
+        <a href="https://bit.ly/2QB3hoH">GDLWEBCAMP (Proyecto en desarrollo)</a> 
+        <!-- <a href="Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)\Big Project\index.html">GDLWEBCAMP (Proyecto en desarrollo)</a> -->
+        <div class="descripcion">
+          <p> Este proyecto es desarrollado como practica en el desarrollo web por medio de PHP</p>
+        </div>
         </div>
         <div class="proyecto">
         <a href="https://bit.ly/2ODUaSq">UNCalendario</a>
+        <div class="descripcion">
+        <p> Este proyecto desarrollado como aplicacion para uso de la comunidad universitaria de la Universidad Nacional de Colombia, donde se pueden crear eventos y compartirlos con todos us contactos.</p>
+        </div>
         </div>
         <div class="proyecto">
         <a href="https://bit.ly/2JZf6CI">ISI Inmobiliaria</a>
+        <div class="descripcion">
+        <p> Esta pagina fue desarrolada como ejemplo de diseño web comercial  y practica de habilidades.</p>
+        </div>
         </div>
     </div>
 
