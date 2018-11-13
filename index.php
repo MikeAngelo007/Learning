5f8ebcffe4e574804dab50b9e10ad4fb16e51634
Miguel Tovar <matovaro@unal.edu.co>
2018-11-12 22:45:52 -0500

    Avances nocturnos

--- index.php
+++ index.php
@@ -22,25 +22,32 @@
             
               
         </div>
-        <div class="proyecto">
-        <a href="https://bit.ly/2QB3hoH">GDLWEBCAMP (Proyecto en desarrollo)</a> 
-        <!-- <a href="Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)\Big Project\index.html">GDLWEBCAMP (Proyecto en desarrollo)</a> -->
-        <div class="descripcion">
-          <p> Este proyecto es desarrollado como practica en el desarrollo web por medio de PHP</p>
-        </div>
-        </div>
-        <div class="proyecto">
-        <a href="https://bit.ly/2ODUaSq">UNCalendario</a>
-        <div class="descripcion">
-        <p> Este proyecto desarrollado como aplicacion para uso de la comunidad universitaria de la Universidad Nacional de Colombia, donde se pueden crear eventos y compartirlos con todos us contactos.</p>
-        </div>
-        </div>
-        <div class="proyecto">
-        <a href="https://bit.ly/2JZf6CI">ISI Inmobiliaria</a>
-        <div class="descripcion">
-        <p> Esta pagina fue desarrolada como ejemplo de diseño web comercial  y practica de habilidades.</p>
-        </div>
-        </div>
+        <a href="https://bit.ly/2QB3hoH">
+          <div class="proyecto">
+            <h2>GDLWEBCAMP (Proyecto en desarrollo)</h2>
+            <!-- <a href="Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)\Big Project\index.html">GDLWEBCAMP (Proyecto en desarrollo)</a> -->
+            <div class="descripcion">
+              <p> Este proyecto es desarrollado como practica en el desarrollo web por medio de PHP</p>
+            </div>
+          </div>
+        </a>
+        <a href="https://bit.ly/2ODUaSq">
+          <div class="proyecto">
+            <h2>UNCalendario</h2>
+            <div class="descripcion">
+              <p> Este proyecto desarrollado como aplicacion para uso de la comunidad universitaria de la Universidad Nacional de Colombia, donde se pueden crear eventos y compartirlos con todos us contactos.</p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://bit.ly/2JZf6CI">
+          <div class="proyecto">
+          <h2>ISI Inmobiliaria</h2>
+            <div class="descripcion">
+              <p> Esta pagina fue desarrolada como ejemplo de diseño web comercial  y practica de habilidades.</p>
+            </div>
+          </div>
+        </a>
     </div>
 
     
