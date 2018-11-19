1d81b1a1ec9642a000e73a03cd8e46c4c26c378a
Miguel Tovar <matovaro@unal.edu.co>
2018-11-19 01:07:36 -0500

    Avances form php

---
+++ Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/PHP/form/formularios/respuesta.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <div class="contenedor">
+      <h1>Aprendiendo PHP</h1>
+
+        <?php $resultado = $_POST; ?>
+        <?php $nombre = $resultado["nombre"];
+              $apellido = $resultado["apellido"];
+        ?>
+
+
+        <?php 
+            if(!(filter_has_var(INPUT_POST, 'nombre')&& (strlen(filter_input(INPUT_POST, 'nombre')) > 0)) ){
+                echo 'Debes agregar un valor';
+            }else{?>
+            <p> Nombre: <?php echo $nombre; ?> </p>
+            
+            <?php    };
+        ?>
+
+        <?php if(!isset($apellido) || trim($apellido) != ''){ ?>
+            <p> Apellido: <?php echo $apellido; ?> </p>
+
+        <?php }else{
+
+            echo "El apellido es obligatorio";
+        }
+
+        ?>
+        
+        
+        
+    </div>
+
+
+
+
+  </body>
+</html>
