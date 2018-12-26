d04a196a81ab701419dd8a98984fcdf8e5c215a1
Miguel Tovar <matovaro@unal.edu.co>
2018-12-26 00:37:06 -0500

    Agenda temrinada total

--- Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/ProyectoAgendaContactos/inc/layout/footer.php
+++ Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/ProyectoAgendaContactos/inc/layout/footer.php
@@ -1,3 +1,16 @@
+<div class="contenedor-barra footer">
+    <div class="columns">
+        <div class="2col"><h3>Agenda de contactos. Miguel Tovar &copy 2018</h3></div>
+        <div class="2col">
+            <a href="https://www.facebook.com/M1K3VN63L0"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/mikeangelo1107/"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.github.com/mikeangelo007"><i class="fab fa-github"></i></a>
+            <a href="https://mikeangelo007.github.io"><i class="fas fa-globe"></i></a>
+            <a href="mailto:matovaro@unal.edu.co"><i class="fas fa-at"></i></a>
+        </div>
+    </div>
+
+</div>
 <script src="js/app.js"></script>
 </body>
 </html>
