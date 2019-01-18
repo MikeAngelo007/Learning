909cc2a224e76f1a02830123343078bf1dde819d
Miguel Tovar <matovaro@unal.edu.co>
2019-01-18 01:53:41 -0500

    Fin Paypal intro

--- Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/paypal/config.php
+++ Desarrollo completo (PHP, JS, AJAX, MySQL, HTML5, CSS3, jQuery)/paypal/config.php
@@ -1,6 +1,6 @@
 <?php
 require 'paypal/autoload.php';
-
+define('URL_SITIO','http://localhost/learning/Desarrollo%20completo%20(PHP,%20JS,%20AJAX,%20MySQL,%20HTML5,%20CSS3,%20jQuery)/paypal');
 $apiContext = new \PayPal\Rest\apiContext(
     new \PayPal\Auth\OAuthTokenCredential(
         'AU0Pfhcy9M3dIUBF-PdPbPKtgYmJz8SC2h4OTToHSJgrVNnaBFRqbLax0SfeNpihyueixjnrwo1Hj9jQ',//ClienteID
